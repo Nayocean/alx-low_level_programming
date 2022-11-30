3e5d5e308cd2e0c81c2388ea9f51cc775c3938f2
Nayocean <Goodyearrotiba>
2022-11-29 21:15:14 -0800

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * * find_listint_loop - finds a loop in a linked list
- * *
- * * @head: linked list to search
- * *
- * * Return: address of node where loop starts/returns, NULL if no loop
- * */
+ * find_listint_loop - finds a loop in a linked list
+ *
+ * @head: linked list to search
+ *
+ * Return: address of node where loop starts/returns, NULL if no loop
+ */
 
 listint_t *find_listint_loop(listint_t *head)
 {
